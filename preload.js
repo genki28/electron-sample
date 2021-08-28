8aae3781c93943f077c6687c01431fe20c93e7c4
GenkiOsawa <taiiku39@gmail.com>
2021-08-28 15:30:22 +0900

    テキスト保存

--- preload.js
+++ preload.js
@@ -1,14 +1,23 @@
 // import { remote, ipcRenderer } from "electron";
-const { remote, ipcRenderer } = require("electron");
-const { dialog } = remote;
+// const { remote, ipcRenderer } = require("electron");
+// const { dialog } = remote;
 // const { dialog } = remote;
 // import fs from "fs";
 
 // // tsの場合、windowはどうなる？？
-window.remote = remote;
-window.MenuItem = remote.MenuItem;
-window.ipcRenderer = ipcRenderer;
-window.dialog = dialog;
-// window.dialog = dialog;
+// window.remote = remote;
+// window.MenuItem = remote.MenuItem;
 // window.ipcRenderer = ipcRenderer;
-// window.fs = fs;
+// window.dialog = dialog;
+// // window.dialog = dialog;
+// // window.ipcRenderer = ipcRenderer;
+// // window.fs = fs;
+
+const { remote, ipcRenderer } = require("electron");
+const { dialog } = remote;
+const fs = require("fs");
+
+window.remote = remote;
+window.dialog = dialog;
+window.ipcRenderer = ipcRenderer;
+window.fs = fs;
