fe0971367ad76fa7aa7f305f41672359aa97b7fb
GenkiOsawa <taiiku39@gmail.com>
2021-08-28 17:14:24 +0900

    sqlite系

--- preload.js
+++ preload.js
@@ -16,8 +16,15 @@
 const { remote, ipcRenderer } = require("electron");
 const { dialog } = remote;
 const fs = require("fs");
+const https = require("https")
+const sqlite3 = require("sqlite3")
+const path = require("path");
 
 window.remote = remote;
 window.dialog = dialog;
 window.ipcRenderer = ipcRenderer;
 window.fs = fs;
+window.https = https;
+window.sqlite3 = sqlite3;
+window.path = path;
+window.dirname = __dirname;
